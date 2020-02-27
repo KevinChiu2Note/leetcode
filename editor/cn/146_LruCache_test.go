c0d68b85069a7db07718fc1d0e9794c8f5ff71ef
Kevin.Chiu <Kevin.Chiu.free@gmail.com>
2020-02-27 12:05:56 +0800

    optimize get func

--- editor/cn/146_LruCache_test.go
+++ editor/cn/146_LruCache_test.go
@@ -7,15 +7,15 @@ import (
 
 //运用你所掌握的数据结构，设计和实现一个 LRU (最近最少使用) 缓存机制。它应该支持以下操作： 获取数据 get 和 写入数据 put 。
 //
-// 获取数据 get(key) - 如果密钥 (key) 存在于缓存中，则获取密钥的值（总是正数），否则返回 -1。 
+// 获取数据 get(key) - 如果密钥 (key) 存在于缓存中，则获取密钥的值（总是正数），否则返回 -1。
 //写入数据 put(key, value) - 如果密钥不存在，则写入其数据值。当缓存容量达到上限时，它应该在写入新数据之前删除最近最少使用的数据值，从而为新
-//的数据值留出空间。 
+//的数据值留出空间。
 //
-// 进阶: 
+// 进阶:
 //
-// 你是否可以在 O(1) 时间复杂度内完成这两种操作？ 
+// 你是否可以在 O(1) 时间复杂度内完成这两种操作？
 //
-// 示例: 
+// 示例:
 //
 // LRUCache cache = new LRUCache( 2 /* 缓存容量 */ );
 //
@@ -28,7 +28,7 @@ import (
 //cache.get(1);       // 返回 -1 (未找到)
 //cache.get(3);       // 返回  3
 //cache.get(4);       // 返回  4
-// 
+//
 // Related Topics 设计
 
 //leetcode submit region begin(Prohibit modification and deletion)
@@ -109,7 +109,15 @@ func Constructor(capacity int) LRUCache {
 
 func (this *LRUCache) Get(key int) int {
 	if v, ok := this.hashMap[key]; ok {
-		this.Put(key, v.data)
+		// 优化，将查询的节点直接移到最前端
+		//BenchMark 520 ns/op -> 320 ns/op
+		//this.Put(key, v.data)
+		v.preNode.nextNode = v.nextNode
+		v.nextNode.preNode = v.preNode
+		v.nextNode = this.doubleLinked.headNode.nextNode
+		v.preNode = this.doubleLinked.headNode
+		this.doubleLinked.headNode.nextNode = v
+		v.nextNode.preNode = v
 		return v.data
 	} else {
 		return -1
@@ -158,4 +166,19 @@ func TestLruCache(t *testing.T) {
 	assert.Equal(t, cache.Get(3), 3)
 	assert.Equal(t, cache.Get(4), 4)
 
+}
+
+func BenchmarkLruCache(b *testing.B) {
+	cache := Constructor(2)
+	for i := 0; i < b.N; i++ {
+		cache.Put(1, 1)
+		cache.Put(2, 2)
+		cache.Get(1)
+		cache.Put(3, 3)
+		cache.Get(2)
+		cache.Put(4, 4)
+		cache.Get(1)
+		cache.Get(3)
+		cache.Get(4)
+	}
 }
